663f28ab5b12b8e9ee14591831a8568e5ff2d47b
marcioviegas <marcioaugustoviegas@gmail.com>
2020-02-22 15:23:42 -0300

    Add catch on api call

--- src/api/MergeRequestApi.ts
+++ src/api/MergeRequestApi.ts
@@ -13,47 +13,57 @@ export default class MergeRequestApi {
     projectId: string,
     mergeRequestId: number
   ): Promise<PullRequest> {
-    const mergeRequest: any = await this.client.MergeRequests.show(
-      projectId,
-      mergeRequestId
-    );
+    try {
+      const mergeRequest: any = await this.client.MergeRequests.show(
+        projectId,
+        mergeRequestId
+      );
 
-    const pullRequest: PullRequest = new PullRequest(
-      mergeRequest.created_at,
-      mergeRequest.author.username,
-      mergeRequest.state,
-      mergeRequest.targed_branch,
-      mergeRequest.source_branch,
-      mergeRequest.title,
-      mergeRequest.description
-    );
+      const pullRequest: PullRequest = new PullRequest(
+        mergeRequest.created_at,
+        mergeRequest.author.username,
+        mergeRequest.state,
+        mergeRequest.targed_branch,
+        mergeRequest.source_branch,
+        mergeRequest.title,
+        mergeRequest.description
+      );
 
-    const discussions: any = await this.client.MergeRequestDiscussions.all(
-      projectId,
-      mergeRequestId
-    );
+      const discussions: any = await this.client.MergeRequestDiscussions.all(
+        projectId,
+        mergeRequestId
+      );
 
-    pullRequest.addEvents(
-      discussions.map(d => {
-        return new Discussion(
-          d.notes.map(n => {
-            return new Note(n.created_at, n.author.username, n.body, n.system);
-          })
-        );
-      })
-    );
+      pullRequest.addEvents(
+        discussions.map(d => {
+          return new Discussion(
+            d.notes.map(n => {
+              return new Note(
+                n.created_at,
+                n.author.username,
+                n.body,
+                n.system
+              );
+            })
+          );
+        })
+      );
 
-    const commits: any = await this.client.MergeRequests.commits(
-      projectId,
-      mergeRequestId
-    );
+      const commits: any = await this.client.MergeRequests.commits(
+        projectId,
+        mergeRequestId
+      );
 
-    pullRequest.addEvents(
-      commits.map(c => {
-        return new Commit(c.created_at, c.commiter_name, c.message);
-      })
-    );
+      pullRequest.addEvents(
+        commits.map(c => {
+          return new Commit(c.created_at, c.commiter_name, c.message);
+        })
+      );
 
-    return pullRequest;
+      return pullRequest;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   }
 }
